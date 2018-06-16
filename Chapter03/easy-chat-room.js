8faaa2e2ba21dbeb3d3565bb580e2a41103581e4
ironmax <ironmax@163.com>
2018-06-16 20:52:47 +0800

    update

--- Chapter03/easy-chat-room.js
+++ Chapter03/easy-chat-room.js
@@ -5,17 +5,19 @@ var channel = new EventEmitter();
 channel.clients = {};
 channel.subscriptions = {};
 channel.on('join', function(id, client){
-  console.log('join: '+id);
   this.clients[id] = client;
-  this.subscriptions[id] = function(clientID, msg){
-    console.log(clientID);
-    console.log(msg);
-    if (id !== clientID) {
-      this.clients[clientID].write(msg);
+  this.subscriptions[id] = function(senderID, msg){
+    if (id !== senderID) {
+      this.clients[id].write(msg);
     }
   };
 
   this.on('broadcast', this.subscriptions[id]);
+  this.on('leave', function(sid){
+    console.log(channel.subscriptions[sid]);
+    channel.removeListener('broadcast', channel.subscriptions[sid]);
+    channel.emit('broadcast', sid, sid+' has left the chat room.\r\n');
+  });
 });
 
 var server = net.createServer(function(client){
@@ -23,6 +25,11 @@ var server = net.createServer(function(client){
   channel.emit('join', id, client);
   client.on('data', function(data){
     channel.emit('broadcast', id, data);
+  });
+  client.on('close', function(){
+    console.log('有人断开连接');
+    // 用户断开连接时发出 leave 事件
+    channel.emit('leave', id);
   });
 });
 // server.on('connection', function(client){
