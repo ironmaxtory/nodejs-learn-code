84895703c65c2832e1fff221bb0e8ca0c69fd0a6
ironmax <ironmax@163.com>
2018-06-19 09:32:40 +0800

    update

--- Chapter03/easy-chat-room.js
+++ Chapter03/easy-chat-room.js
@@ -13,29 +13,43 @@ channel.on('join', function(id, client){
   };
 
   this.on('broadcast', this.subscriptions[id]);
-  this.on('leave', function(sid){
-    console.log(channel.subscriptions[sid]);
-    channel.removeListener('broadcast', channel.subscriptions[sid]);
-    channel.emit('broadcast', sid, sid+' has left the chat room.\r\n');
+  this.on('leave', function(senderID){
+    if (id === senderID) {
+      this.removeListener('broadcast', this.subscriptions[senderID]);
+      this.emit('broadcast', senderID, '['+senderID+']'+' has left the chat room.\r\n');
+    }
   });
+});
+channel.on('shutdown', function(){
+  console.log('关闭所有连接');
+  this.emit('broadcast', '', 'Chat room has been shutdown!');
+  this.removeAllListeners('broadcast');
 });
 
 var server = net.createServer(function(client){
   var id = client.remoteAddress + ':' + client.remotePort;
+  console.log('[' + id + ']' + ' 已连接');
+
+  // 加入 channel
   channel.emit('join', id, client);
+
+  // 接收到数据后，广播给其他用户
   client.on('data', function(data){
+    data = data.toString();
+    console.log(data.length);
+    if (data.length > 2 && data.slice(0, -2) === 'shutdown') {
+      channel.emit('shutdown');
+    }
     channel.emit('broadcast', id, data);
   });
+
+  // 关闭时，离开 channel
   client.on('close', function(){
-    console.log('有人断开连接');
+    console.log('[' + id + ']' + ' 已关闭');
     // 用户断开连接时发出 leave 事件
     channel.emit('leave', id);
   });
 });
-// server.on('connection', function(client){
-//   var id = client.remoteAddress + ':' + client.remotePort;
-//   channel.emit('join', id, client);
-// });
 
 server.listen(5020, function(){
   console.log('An easy server for chating is listening on port 5020 ...');
--- Chapter03/echo_server.js
+++ Chapter03/echo_server.js
@@ -1,10 +1,19 @@
 var net = require('net');
 
 var server = net.createServer(function(socket) {
+  var id = socket.remoteAddress + ':' + socket.remotePort;
+  
+  console.log('[' + id + ']' + ' 已连接');
+
   socket.on('data', function(data) {
     socket.write(data);
   });
+
+  socket.on('close', function(){
+    console.log('[' + id + ']' + ' 已关闭');
+  });
 });
+
 
 server.listen(5020, function() {
   console.log('Echo Server is listening on port 5020 ...');
